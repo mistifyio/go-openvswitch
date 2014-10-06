5e6feb819ac6148cbdaa893c3f97b20fa75ed279
Corey Cossentino <corey@omniti.com>
2014-10-06 16:22:18 -0400

    MIST-117: add some more commands to ovs interface

--- ovs.go
+++ ovs.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"errors"
 	"os/exec"
+	"strings"
 )
 
 func run(cmd string, arg ...string) (bytes.Buffer, bytes.Buffer, error) {
@@ -20,6 +21,64 @@ func run(cmd string, arg ...string) (bytes.Buffer, bytes.Buffer, error) {
 
 func AddBridge(name string) error {
 	_, stderr, err := run("ovs-vsctl", "add-br", name)
+	if err != nil {
+		errmsg := stderr.String()
+		return errors.New(errmsg)
+	}
+
+	return nil
+}
+
+func ListBridges() ([]string, error) {
+	var bridges []string
+
+	stdout, stderr, err := run("ovs-vsctl", "list-br")
+	if err != nil {
+		errmsg := stderr.String()
+		return bridges, errors.New(errmsg)
+	}
+
+	bridges = strings.Split(stdout.String(), "\n")
+	bridges = bridges[:len(bridges)-1] // Remove empty line from end
+	return bridges, nil
+}
+
+func DeleteBridge(name string) error {
+	_, stderr, err := run("ovs-vsctl", "del-br", name)
+	if err != nil {
+		errmsg := stderr.String()
+		return errors.New(errmsg)
+	}
+
+	return nil
+}
+
+func AddPort(bridge string, port string) error {
+	_, stderr, err := run("ovs-vsctl", "add-port", bridge, port)
+	if err != nil {
+		errmsg := stderr.String()
+		return errors.New(errmsg)
+	}
+
+	return nil
+}
+
+func ListPorts(bridge string) ([]string, error) {
+	var ports []string
+
+	stdout, stderr, err := run("ovs-vsctl", "list-ports", bridge)
+	if err != nil {
+		errmsg := stderr.String()
+		return ports, errors.New(errmsg)
+	}
+
+	ports = strings.Split(stdout.String(), "\n")
+	ports = ports[:len(ports)-1] // Remove empty line from end
+	return ports, nil
+}
+
+func DeletePort(bridge string, port string) error {
+	_, stderr, err := run("ovs-vsctl", "del-port", bridge, port)
 	if err != nil {
 		errmsg := stderr.String()
 		return errors.New(errmsg)
--- ovs_test.go
+++ ovs_test.go
@@ -6,9 +6,70 @@ import (
 	"testing"
 )
 
-func TestAddBridge(t *testing.T) {
-	if err := ovs.AddBridge("br0"); err != nil {
+func TestCommands(t *testing.T) {
+	var err error
+
+	// AddBridge
+	if err = ovs.AddBridge("br0"); err != nil {
 		fmt.Printf("Add bridge failed: %s", err.Error())
+		t.FailNow()
+	}
+
+	// ListBridges
+	bridges, err := ovs.ListBridges()
+
+	if err != nil {
+		fmt.Printf("List bridges failed: %s", err.Error())
+		t.FailNow()
+	}
+
+	if len(bridges) != 1 {
+		fmt.Printf("List bridges returned %d bridges (should be 1).", len(bridges))
+		t.FailNow()
+	}
+
+	// AddPort
+	if err = ovs.AddPort("br0", "eth1"); err != nil {
+		fmt.Printf("Add port failed: %s", err.Error())
+		t.FailNow()
+	}
+
+	// ListPorts
+	ports, err := ovs.ListPorts("br0")
+
+	if err != nil {
+		fmt.Printf("List ports failed: %s", err.Error())
+		t.FailNow()
+	}
+
+	if len(ports) != 1 {
+		fmt.Printf("List ports returned no ports.")
+		t.FailNow()
+	}
+
+	// DeletePort
+	if err = ovs.DeletePort("br0", "eth1"); err != nil {
+		fmt.Printf("Delete port failed: %s", err.Error())
+		t.FailNow()
+	}
+
+	ports, err = ovs.ListPorts("br0")
+
+	if len(ports) != 0 {
+		fmt.Printf("%d ports exist on br0 after delete (should be 0)", len(ports))
+		t.FailNow()
+	}
+
+	// DeleteBridge
+	if err = ovs.DeleteBridge("br0"); err != nil {
+		fmt.Printf("Delete bridge failed: %s", err.Error())
+		t.FailNow()
+	}
+
+	bridges, err = ovs.ListBridges()
+
+	if len(bridges) != 0 {
+		fmt.Printf("%d bridges exist after delete (should be 0)", len(bridges))
 		t.FailNow()
 	}
 }
